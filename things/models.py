f7de1a21795bc38d18d5dca2819c9fbb929f3d09
Fazza Bajwa <fazza.bajwa@kcl.ac.uk>
2022-10-21 13:53:49 +0100

    Extending things model.(saved version)

--- things/models.py
+++ things/models.py
@@ -1,4 +1,22 @@
-from django.db import models
+rom django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
-class Thing:
-    pass
+class Thing(models.Model):
+    name = models.CharField(
+        max_length=30,
+        unique=True,
+        blank=False,
+        default="thing"
+    )
+    description = models.CharField(
+        max_length=120,
+        unique=False,
+        blank=True
+    )
+    quantity = models.IntegerField(
+        unique=False,
+        validators = [
+            MinValueValidator(0),
+            MaxValueValidator(100)
+        ]
+    )
