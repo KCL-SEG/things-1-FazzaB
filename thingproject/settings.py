3534a2639b377e2f419e97dd43b6bcaaf2ac4aa0
Fazza Bajwa <fazza.bajwa@kcl.ac.uk>
2022-10-21 13:38:04 +0100

    Registering app in settings page.

--- thingproject/settings.py
+++ thingproject/settings.py
@@ -123,4 +123,4 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-AUTH_USER_MODEL = 'Things.thing'
+AUTH_THINGS_MODEL = 'Things.thing'
